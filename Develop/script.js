ada2558013fe2d647120e03495df5ebb0d2f59e1
Tonyoffor <tonyc.offor@gmail.com>
2022-10-28 15:05:51 -0400

    added more arrays

--- Develop/script.js
+++ Develop/script.js
@@ -2,19 +2,24 @@
 var password = document.querySelector("#password")
 var generate = document.querySelector("#generate") 
 
-const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword()
-
-
-function writePassword() {
-  var password = generatePassword();
+var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+
+
+const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+const numeric = [ '1', '2','3', '4', '5', '6', '7', '8', '9', '0']
+const specialcharacters = [ '~', '!', '@', '#', '$', '%', '^', '&', '*']
+
+function writePassword() {
+  
 }
 
   generateBtn.addEventListener("click", writePassword)
