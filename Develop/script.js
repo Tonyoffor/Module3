b694f1f191ec8f5fa6d81b87dc2b9a4dcc3741c9
Tonyoffor <tonyc.offor@gmail.com>
2022-10-28 14:32:11 -0400

    added some array's and still editing

--- Develop/script.js
+++ Develop/script.js
@@ -2,19 +2,20 @@
 var password = document.querySelector("#password")
 var generate = document.querySelector("#generate") 
 
-counter = 0
-
+const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword()
+
+
 function writePassword() {
-  if(counter > 0){
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-} else 
-
+}
 
   generateBtn.addEventListener("click", writePassword)
 
@@ -32,5 +33,5 @@ function writePassword() {
 
 // add an event listener on click
 
-}
+
 
