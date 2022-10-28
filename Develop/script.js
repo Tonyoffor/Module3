3f944c9d8b76bde6122d93d9a024a460943e21df
Tonyoffor <tonyc.offor@gmail.com>
2022-10-28 07:31:39 -0400

    added notes and some inital varibales

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment code here
-var generate
+var generatePassword = 
 var password
+var expression 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -12,8 +13,19 @@ function writePassword() {
 
   passwordText.value = password;
 
-  //  if(password) = < 8 or > 128
-  //console.log("Please make sure password is between 8 to 128 characters")
+  //  if(password) (=> 8, =< 128) then true ; if false then console.log("Please make sure password is between 8 to 128 characters")
+  //if(password == =>8, =<128){
+// Console.log("generatePassword");
+  //}else console.log Please make sure password is between 8 to 128 character
+
+  // if password => 1 text.uppercase then true
+  // The password is true if it has all these factors are true; I should have some funky looking && && mix then generate a math.random password that meets all criteria
+
+  //function validate(obj, lowval, hival) {
+ // if ((obj.value < lowval) || (obj.value > hival)) {
+   // console.log('Invalid Value!');
+
+// add an event listener on click
 
 }
 
