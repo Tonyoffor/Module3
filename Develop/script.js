7bf40714d41e0c9fb59168aab1ed4b247a4a94be
Tonyoffor <tonyc.offor@gmail.com>
2022-10-28 17:25:10 -0400

    added eventlistener to button

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,14 @@
 // Assignment code here
 var password = document.querySelector("#password")
 var generate = document.querySelector("#generate") 
+const button = document.getElementById("button")
 
+button.addEventListener('click', startPrompts)
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");{
+  eliminateCommas
+}
 
 // Write password to the #password input
 function generatePassword()
@@ -15,13 +19,13 @@ var password = generatePassword();
 
 const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-const numeric = [ '1', '2','3', '4', '5', '6', '7', '8', '9', '0']
+const numeric = ['1', '2','3', '4', '5', '6', '7', '8', '9', '0']
 const specialcharacters = [ '~', '!', '@', '#', '$', '%', '^', '&', '*']
 
-function writePassword() {
-  
-}
+// if (promptLength < 8 || promptLength > 128){
+   //   ("Please choose a number between 8 and 128 characters");
 
+   function writePassword() 
   generateBtn.addEventListener("click", writePassword)
 
   //  if(password) (=> 8, =< 128) then true ; if false then console.log("Please make sure password is between 8 to 128 characters")
